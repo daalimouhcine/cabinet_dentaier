b259804b45cabf4672569cac431fd519ba18a35b
Mouhcine Daali <daalim277@gmail.com>
2022-03-30 06:52:07 +0000

    work on patients controller

--- Back/APIs/Patients.php
+++ Back/APIs/Patients.php
@@ -1,10 +1,28 @@
 <?php
 
-    class Patients {
-        public function index() {
-            echo 'getAll';
+    // Set the API for patient to be called
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+    class Patients extends Controller {
+        public function __construct() {
+            $this->patientModel = $this->model('Patient');
         }
-        public function hello() {
-            echo 'hello';
+
+        public function getAll() {
+            echo json_encode($this->patientModel->getAll());
+        }
+        public function create() {
+            $patientData = [
+                'id' =>  uniqid('patient_'),
+                'user_name' => $_POST['user_name'],
+                'email' => $_POST['email'],
+                'first_name' => $_POST['first_name'],
+                'last_name' => $_POST['last_name'],
+                'birth_date' => $_POST['birth_date'],
+                'gender' => $_POST['gender']
+            ];
+            $this->patientModel->createP($patientData);
+
         }
     }
