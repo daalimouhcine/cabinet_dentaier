7ffb270c3893cfc0a115ddd52170a503cb6f697d
Mouhcine Daali <daalim277@gmail.com>
2022-03-31 10:09:29 +0000

    add form file to do the login and registre components

---
+++ Front/src/components/register/Form.jsx
@@ -0,0 +1,12 @@
+import FormSignUp from "./FormSignUp";
+import './SASS/Form.css';
+
+const Form = () => {
+    return (
+        <div className="form">
+            <FormSignUp />
+        </div>
+    )
+}
+
+export default Form;
---
+++ Front/src/components/register/FormSignUp.jsx
@@ -0,0 +1,10 @@
+
+const FormSignUp = () => {
+    return (
+        <div className="form-signup">
+            <h1 className="font-bold text-3xl">Sign Up</h1>
+        </div>
+    )
+}
+
+export default FormSignUp;
