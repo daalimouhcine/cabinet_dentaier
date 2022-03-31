9bd5a8861b68b04b0538d23ecb3853e89546818c
Mouhcine Daali <daalim277@gmail.com>
2022-03-31 10:14:52 +0000

    all the basic files to work with react

---
+++ Front/src/App.jsx
@@ -0,0 +1,11 @@
+import Form from './components/register/Form';
+
+const App = () => {
+    return (
+        <div>
+            <Form />
+        </div>
+    )
+}
+
+export default App;
---
+++ Front/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+
+const root = document.getElementById('root');
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    , root
+);
