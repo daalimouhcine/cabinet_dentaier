f9e789356bcf676263a92fb2bb1ce2cdbe7be9c4
Mouhcine Daali <daalim277@gmail.com>
2022-03-31 10:13:42 +0000

    create login component

---
+++ Front/src/components/logIn/logIn.jsx
@@ -0,0 +1,10 @@
+
+const logIn = () => {
+    return (
+        <div className="logIn">
+            <h1 className="font-bold text-3xl">Log In</h1>
+        </div>
+    )
+}
+
+export default logIn;
