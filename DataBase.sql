f8cc6eb8ad05281489195013a077d106d7e6f0d4
Mouhcine Daali <daalim277@gmail.com>
2022-03-31 10:05:48 +0000

    modify databse

--- DataBase.sql
+++ DataBase.sql
@@ -1,27 +1,26 @@
-CREATE DATABASE IF NOT EXISTS cabinet_dentaire ;
+CREATE DATABASE IF NOT EXISTS cabinet_dentaire;
 
 CREATE TABLE IF NOT EXISTS admin (
     user_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
-),
+);
 
 CREATE TABLE IF NOT EXISTS patients (
-    id INT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
+    id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     birth_date DATE NOT NULL,
     gender VARCHAR(255) NOT NULL
-),
+);
 
 CREATE TABLE IF NOT EXISTS appointments (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    patient_id INT NOT NULL,
+    patient_id VARCHAR(255) NOT NULL,
     date_time DATETIME NOT NULL,
     description VARCHAR(255) NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE
-),
+    FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE VIEW patient_appointments AS SELECT appointments.id AS appointment_id, appointments.date_time, appointments.description, appointments.patient_id, patients.user_name, patients.email, patients.first_name, patients.last_name, patients.birth_date, patients.gender FROM appointments INNER JOIN patients ON appointments.patient_id = patients.id; 
+CREATE VIEW patient_appointments AS SELECT appointments.id AS appointment_id, appointments.date_time, appointments.description, appointments.patient_id, patients.email, patients.first_name, patients.last_name, patients.birth_date, patients.gender FROM appointments INNER JOIN patients ON appointments.patient_id = patients.id; 
