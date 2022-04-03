a2b951df60fd9896230f6935499ae2adc0c50a90
Mouhcine Daali <daalim277@gmail.com>
2022-04-03 00:20:48 +0000

    add some classes

--- Front/src/components/register/Form.jsx
+++ Front/src/components/register/Form.jsx
@@ -5,14 +5,16 @@ const Form = () => {
     return (
         <div className="mx-auto max-w-7xl">
             <div className="flex flex-col lg:flex-row">
-                <div className="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg/gradient-to-r from-white via-white to-gray-100">
-                    <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                        <div className="relative">
-                            <p className="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
-                            <h2 className="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you work smarter</h2>
+                <div className="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+                    <div className="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+                        <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+                            <div className="relative">
+                                <p className="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
+                                <h2 className="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you work smarter</h2>
+                            </div>
+                            <p className="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business and your application.</p>
+                            <a href="#_" className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Get Started Today</a>
                         </div>
-                        <p className="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business and your application</p>
-                        <a href="#_" className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:gb-blue-700 ease">Get started today</a>
                     </div>
                 </div>
                 <FormSignUp />
