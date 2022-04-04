336e1bd782a4b0587c5e4cff4657692d6b014780
Mouhcine Daali <daalim277@gmail.com>
2022-04-04 14:32:50 +0000

    add condition to the iputs part

--- Front/src/components/register/Form.jsx
+++ Front/src/components/register/Form.jsx
@@ -1,7 +1,14 @@
+import {useState} from 'react';
 import FormSignUp from "./FormSignUp";
-
+import FormSuccess from "./FormSuccess";
 
 const Form = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function submitForm() {
+        setIsSubmitted(true);
+    }
+
     return (
         <div className="mx-auto max-w-7xl">
             <div className="flex flex-col lg:flex-row">
@@ -17,7 +24,7 @@ const Form = () => {
                         </div>
                     </div>
                 </div>
-                <FormSignUp />
+                {!isSubmitted ? <FormSignUp submitForm={submitForm} /> : <FormSuccess />}
             </div>
         </div>
     )
