f1aaa85e6aa59f185d6eca7dc5bb609f40247f67
Mouhcine Daali <daalim277@gmail.com>
2022-04-04 14:30:23 +0000

    delete the input component

--- Front/src/components/register/FormSignUp.jsx
+++ Front/src/components/register/FormSignUp.jsx
@@ -1,42 +1,131 @@
-import useForm from './useForm';
-
-const Inputs = (props) => {
-    const {handleChange, values} = useForm();
-    const inputName = props.name;
-    return (
-        <div className="relative">
-            <label className="font-medium" htmlFor={props.for}>{props.placeholder}</label>
-            <input className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" type={props.type} name={inputName} id={props.id} placeholder={props.placeholder} value={values.inputName} onChange={handleChange} />
-            <span className="text-red-500">{props.errorMessage}</span>
-        </div>
-    )
-}
+import useForm from "./useForm";
+import validate from './validateInfo';
 
 
-const FormSignUp = () => {
-    return (
-        <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
-            <div>
-                <img src="" alt="" srcset="" />
-            </div>
-            <div className="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
-                {/* <div className="title-form">
+const FormSignUp = ({submitForm}) => {
+
+  const { values, handleChange, handleSubmit, errors } = useForm(submitForm, validate);
+
+
+  return (
+    <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
+      <div>
+        <img src="" alt="" srcSet="" />
+      </div>
+      <div className="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
+        {/* <div className="title-form">
                     <img src={require('../img/logo.png')} alt="logo" />
                     <h2>Sign Up</h2>
                 </div> */}
-                <h4 className="w-full text-3xl font-bold">Sign Up</h4>
-                <form action="" className="relative w-full mt-10 space-y-8">
-                    <Inputs for="first_name" type="text" name="first_name" id="first_name" placeholder="First Name" errorMessage="" />
-                    <Inputs for="last_name" type="text" name="last_name" id="last_name" placeholder="Last Name" errorMessage="" />
-                    <Inputs for="email" type="email" name="email" id="email" placeholder="Email" errorMessage="" />
-                    <Inputs for="birth_date" type="date" name="birth_date" id="birth_date" placeholder="Birth Date" errorMessage="" />
-                    <Inputs for="gender" type="gender" name="gender" id="gender" placeholder="Gender" errorMessage="" />
-                    <button type="submit" className="w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Sign Up</button>
-                    <b className="text-lg text-gray-500">Already have an account? <a href="#_" className="text-blue-600 underline">Login</a></b>
-                </form>
-            </div>
-        </div>
-    )
-}
+        <h4 className="w-full text-3xl font-bold">Sign Up</h4>
+        <form
+          action=""
+          className="relative w-full mt-10 space-y-8"
+          onSubmit={handleSubmit}
+        >
+          <div className="relative">
+            <label className="font-medium" htmlFor="first_name">
+              First Name
+            </label>
+            <input
+              className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+              type="text"
+              name="first_name"
+              id="first_name"
+              placeholder="First Name"
+              value={values.first_name}
+              onChange={handleChange}
+            />
+            {errors.first_name && (
+              <span className="text-red-500">{errors.first_name}</span>
+            )}
+          </div>
+
+          <div className="relative">
+            <label className="font-medium" htmlFor="last_name">
+              Last Name
+            </label>
+            <input
+              className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+              type="text"
+              name="last_name"
+              id="last_name"
+              placeholder="Last Name"
+              value={values.last_name}
+              onChange={handleChange}
+            />
+            {errors.last_name && (
+              <span className="text-red-500">{errors.last_name}</span>
+            )}
+          </div>
+
+          <div className="relative">
+            <label className="font-medium" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <span className="text-red-500">{errors.email}</span>
+            )}
+          </div>
+
+          <div className="relative">
+            <label className="font-medium" htmlFor="birth_date">
+              Birth Date
+            </label>
+            <input
+              className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+              type="date"
+              name="birth_date"
+              id="birth_date"
+              placeholder="Birth Date"
+              value={values.birth_date}
+              onChange={handleChange}
+            />
+            {errors.birth_date && (
+              <span className="text-red-500">{errors.birth_date}</span>
+            )}
+          </div>
+
+          <div className="relative">
+            <label htmlFor="gender">Gender</label>
+            <select
+              id="gender"
+              name="gender"
+              className="w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 gb-gray-200 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 fucus:ring-opacity-50"
+              onChange={handleChange}
+              value={values.gender}
+            >
+              <option value=""></option>
+              <option value="Male">Male</option>
+              <option value="Male">Female</option>
+            </select>
+            <span className="text-red-500">{errors.gender}</span>
+          </div>
+          <button
+            type="submit"
+            className="w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
+          >
+            Sign Up
+          </button>
+          <b className="text-lg text-gray-500">
+            Already have an account?{" "}
+            <a href="#_" className="text-blue-600 underline">
+              Login
+            </a>
+          </b>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default FormSignUp;
