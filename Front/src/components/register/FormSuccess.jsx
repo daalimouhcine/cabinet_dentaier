c8e480ef3911fe0293d66cafc2e3cd4c1e3dacb7
Mouhcine Daali <daalim277@gmail.com>
2022-04-04 23:58:29 +0000

    add text to the success part

--- Front/src/components/register/FormSuccess.jsx
+++ Front/src/components/register/FormSuccess.jsx
@@ -1,7 +1,9 @@
 
 const FormSuccess = () => {
     return (
-        <div>
+        <div className="w-fit h-full text-center">
+            <p className="font-bold pt-2 text-gray-700">The inscription is done successfully</p>
+            <h3 className="text-3xl pt-5 font-bold ">Check your email for the ID</h3>
             <div>
                 <img src="img/done.svg" alt="success" />
             </div>
