0ed2cad71b2c2aa708a4fa3d668fd918cc2c79ff
Mouhcine Daali <daalim277@gmail.com>
2022-04-05 12:00:52 +0000

    modify the design

--- Front/src/components/register/FormSuccess.jsx
+++ Front/src/components/register/FormSuccess.jsx
@@ -1,10 +1,10 @@
 
 const FormSuccess = () => {
     return (
-        <div className="w-fit h-full text-center">
+        <div className="w-fit flex flex-col justify-center mx-auto text-center">
             <p className="font-bold pt-2 text-gray-700">The inscription is done successfully</p>
             <h3 className="text-3xl pt-5 font-bold ">Check your email for the ID</h3>
-            <div>
+            <div className="">
                 <img src="img/done.svg" alt="success" />
             </div>
         </div>
