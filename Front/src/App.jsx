3fd1a64db82c97b88fb003c75c435d60f3e3ed23
Mouhcine Daali <daalim277@gmail.com>
2022-04-05 14:18:39 +0000

    comment registration form and decomment login

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,12 @@
-// import LogIn from './components/login/LogIn';
-import Form from './components/register/Form';
+import LogIn from './components/login/LogIn';
+// import Form from './components/register/Form';
 import './components/register/SASS/Form.css';
 
 const App = () => {
     return (
         <div className='w-full min-h-screen bg-white'>
-            <Form />
-            {/* <LogIn /> */}
+            {/* <Form /> */}
+            <LogIn />
         </div>
     )
 }
