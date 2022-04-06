2b6d88cbb339ddb4517fa411738146638d3d3644
Mouhcine Daali <daalim277@gmail.com>
2022-04-06 04:02:49 +0000

    create a component to containe component of the same folder

---
+++ Front/src/components/appointment/Appointment.jsx
@@ -0,0 +1,11 @@
+import Add from './addAppointments/Add';
+// import Read from './readAppointments/Read';
+
+const Appointment = () => {
+    return (
+        <Add />
+        // <Read />
+    )
+}
+
+export default Appointment;
---
+++ Front/src/components/authentication/Authentication.jsx
@@ -0,0 +1,13 @@
+import LogIn from './login/LogIn';
+// import Form from './register/Form';
+
+const Authentication = () => {
+    return (
+        <div className='w-full min-h-screen bg-white'>
+            {/* <Form /> */}
+            <LogIn />
+        </div>
+    )
+}
+
+export default Authentication;
