13fed85ec4205514a871079cacdba225e81ff115
Mouhcine Daali <daalim277@gmail.com>
2022-04-08 02:41:06 +0000

    contain the add and read pages

--- Front/src/components/appointment/Appointment.jsx
+++ Front/src/components/appointment/Appointment.jsx
@@ -1,10 +1,14 @@
+import Nav from './Nav';
 import Add from './addAppointments/Add';
 // import Read from './readAppointments/Read';
 
 const Appointment = () => {
     return (
-        <Add />
-        // <Read />
+        <>
+            <Nav />
+            <Add />
+            {/* <Read /> */}
+        </>
     )
 }
 
