47f3c7c675a179c108428b3d90270f2d9e3b7ded
Mouhcine Daali <daalim277@gmail.com>
2022-04-08 02:33:53 +0000

    add routing

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,30 @@
-import Authentication from './components/authentication/Authentication';
-// import Appointment from './components/appointment/Appointment';
+import Login from './components/authentication/login/LogIn';
+import Register from './components/authentication/register/Register';
+import AddAppointment from './components/appointment/addAppointments/Add';
+import ReadAppointment from './components/appointment/readAppointments/Read';
 import './components/SASS/Form.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
     return (
-        <Authentication />
-        // <Appointment />
-    )
+        
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/add">
+                    <AddAppointment />
+                </Route>
+                <Route path="/read">
+                    <ReadAppointment />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
