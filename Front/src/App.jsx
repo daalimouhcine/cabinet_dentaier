5fb37e8d6cf999d0ca221e6bb21692324e93389a
Mouhcine Daali <daalim277@gmail.com>
2022-04-08 23:33:16 +0000

    modify classname

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div class="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-indigo-600 to-blue-400">
+    <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-indigo-600 to-blue-400">
       <div className="px-40 py-20 bg-white rounded-md shadow-xl">
         <div className="flex flex-col items-center">
           <h1 className="font-bold text-blue-600 text-9xl">404</h1>
