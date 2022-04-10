5fab853ce94ebc903654318e410130b4271ea027
Mouhcine Daali <daalim277@gmail.com>
2022-04-10 16:08:45 +0000

    import motion

--- Front/src/components/authentication/login/LogIn.jsx
+++ Front/src/components/authentication/login/LogIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { motion } from "framer-motion"
 
 const LogIn = () => {
   const [currentId, setCurrentId] = useState("");
@@ -39,14 +40,15 @@ const LogIn = () => {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center bg-slate-300">
-      <form
-        className="flex flex-col xl:w-4/6 lg:w-3/5 md:w-3/6 p-4 my-auto text-center rounded-md"
+    <div  className="w-full h-screen flex justify-center bg-slate-300">
+      <motion.form
+        animate={{ scale: 1.3 }} transition={{ duration: 0.5 }}
+        className="flex flex-col xl:w-3/6 lg:w-2/5 md:w-2/6 p-4 my-auto text-center rounded-md"
         onSubmit={handleSubmit}
       >
         {error && <span className="text-red-500 text-xs italic">{error}</span>}
         <input
-          className="block w-full p-4 mt-2 text-xl placeholder-gray-400 gb-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+          className="block w-full p-4 mt-2 text-xl placeholder-gray-400 gb-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 md:text-sm"
           type="text"
           name="id"
           id="id"
@@ -55,18 +57,18 @@ const LogIn = () => {
           placeholder="Enter your ID"
         />
         <button
-          className="self-center w-4/12 py-2 mt-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
+          className="self-center w-4/12 py-2 mt-4 md:text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease sm:test-md"
           type="submit"
         >
           Log In
         </button>
-        <b className="text-lg text-gray-500 mt-2">
+        <b className="md:text-md text-gray-500 mt-2 sm:text-md">
           No account?{" "}
           <a href="/register" className="text-blue-600 underline">
             Register
           </a>
         </b>
-      </form>
+      </motion.form>
     </div>
   );
 };
