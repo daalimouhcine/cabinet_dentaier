79714cbb448a259b772b4264e7c7244e4f30726f
Mouhcine Daali <daalim277@gmail.com>
2022-04-10 16:08:59 +0000

    modify content

--- Front/src/components/authentication/register/Register.jsx
+++ Front/src/components/authentication/register/Register.jsx
@@ -17,11 +17,11 @@ const Register = () => {
                         <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
                             <div className="relative">
                                 <p className="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
-                                <h2 className="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you work smarter</h2>
+                                <h2 className="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you take appointments</h2>
                             </div>
-                            <p className="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business and your application.</p>
-                            <a href="#_" className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Get Started Today</a>
-                        </div>
+                            <p className="text-2xl text-gray-700">We've created a simple appointment website in order to facilitate the proses of taking appointment at the dentist.</p>
+                            {!isSubmitted ? (<a href="#_" className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Know more</a>) : (<a href="/" className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">LogIn</a>)}
+                    </div>
                     </div>
                 </div>
                 {!isSubmitted ? <SignUp submitForm={submitForm} /> : <Success />}
